fd5137915d6620d501623922725efa26c5f41e59
zarmstrong <zarmstrong@users.noreply.github.com>
2015-07-12 17:24:38 -0600

    Update fixdwarvenforgeforumimages.user.js
    0.3:
    Added fix_imagedownloads to fix attachments to render them as images on the new forum.

--- fixdwarvenforgeforumimages.user.js
+++ fixdwarvenforgeforumimages.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix Dwarven Forge Forum Images
 // @namespace    http://www.zacharyarmstrong.com/
-// @version      0.2
+// @version      0.3
 // @description  This script fixes images that were broken due to the forum upgrade on dwarven forge forums. 
 // @author       Zach Armstrong
 // @include        http://www.dwarvenforge.com/forum/*
@@ -27,8 +27,24 @@ function remove_badimages(elem,attr)
             elems[i].parentNode.removeChild(elems[i]);
     }
 }
+
+function fix_imagedownloads(elem,attr)
+{
+    var elems = document.getElementsByTagName(elem);
+    for (var i = 0; i < elems.length; i++){
+        var testres = (/http:\/\/(www\.|)dwarvenforge\.com\/forum\/download\/file\.php\?id\=.*/g).test(elems[i][attr]);
+        if (testres === true)
+        {
+            var imagename = elems[i].alt;
+            var newfilename = "".concat('/forum/files/',imagename);
+            console.log ("".concat(imagename, " to become ", newfilename));
+            elems[i][attr] = newfilename;
+        }            
+    }
+}
     
 console.log("Fix Dwarven Forge Forum Images launched");
 replace_url('a', 'href');
 replace_url('img', 'src');
 remove_badimages('img', 'src');
+fix_imagedownloads('img', 'src');
