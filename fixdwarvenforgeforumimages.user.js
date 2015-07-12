a1f34af1017ebac8cc334e01029e3f35ce062c57
zarmstrong <zarmstrong@users.noreply.github.com>
2015-07-12 17:24:20 -0600

    Update fixdwarvenforgeforumimages.user.js
    Added remove_badimages to remove old forum created images that are invalid.

--- fixdwarvenforgeforumimages.user.js
+++ fixdwarvenforgeforumimages.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix Dwarven Forge Forum Images
 // @namespace    http://www.zacharyarmstrong.com/
-// @version      0.1
+// @version      0.2
 // @description  This script fixes images that were broken due to the forum upgrade on dwarven forge forums. 
 // @author       Zach Armstrong
 // @include        http://www.dwarvenforge.com/forum/*
@@ -15,13 +15,20 @@ function replace_url(elem, attr) {
     for (var i = 0; i < elems.length; i++){
         elems[i][attr] = elems[i][attr].replace('http://192.169.214.133/forum/files/', '/forum/files/');
         elems[i][attr] = unescape(elems[i][attr]);
-        
     }
 }
 
-// window.onload = function() {
-    console.log("Fix Dwarven Forge Forum Images launched");
-    replace_url('a', 'href');
-    replace_url('img', 'src');
-    // etc
-// }
+function remove_badimages(elem,attr)
+ {
+    var elems = document.getElementsByTagName(elem);
+    for (var i = 0; i < elems.length; i++){
+        var testres = (/http:\/\/(www\.|)dwarvenforge\.com\/forum\/viewtopic\.php\?f\=.*/g).test(elems[i][attr]);
+        if (testres === true)
+            elems[i].parentNode.removeChild(elems[i]);
+    }
+}
+    
+console.log("Fix Dwarven Forge Forum Images launched");
+replace_url('a', 'href');
+replace_url('img', 'src');
+remove_badimages('img', 'src');
